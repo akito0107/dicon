3cfe5956bd45b95f7dad9f777c2ceffab1f93fad
Nao YONASHIRO <owan.orisano@gmail.com>
2017-11-18 18:54:47 +0900

    refactor: tiny

--- internal/generator.go
+++ internal/generator.go
@@ -93,7 +93,7 @@ func (g *Generator) appendMethod(funcs []FuncType, _ string) {
 }
 
 func (g *Generator) relativePackageName(packageName string) string {
-	if strings.Compare(g.PackageName, packageName) == 0 {
+	if g.PackageName == packageName {
 		return ""
 	}
 
